27e0a5b819c6762781b34d110ce8b53ed998cb68
Ondrej Tesar <ondrej.tesar@domat.cz>
2020-07-31 11:58:34 +0200

    Create menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { PizzaComponent } from './pizza/pizza.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
+  { path: 'pizza', component: PizzaComponent },
+  { path: 'drinks', component: DrinksComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
