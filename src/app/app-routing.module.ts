4536cc47136ce6f8d1bf2c22bb855e3e1a5ee473
Ondrej Tesar <ondrej.tesar@domat.cz>
2020-07-31 12:27:30 +0200

    Add all nav items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DrinksComponent } from './drinks/drinks.component';
 import { PizzaComponent } from './pizza/pizza.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { CareerComponent } from './career/career.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
   { path: 'pizza', component: PizzaComponent },
   { path: 'drinks', component: DrinksComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'career', component: CareerComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
